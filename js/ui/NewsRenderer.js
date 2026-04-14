095091df127f3d84459e0ebf540b4c44dcf9da64
neutrall85 <canni85@mail.ru>
2026-04-14 19:08:00 +0400

    news and minor fixies

--- js/ui/NewsRenderer.js
+++ js/ui/NewsRenderer.js
@@ -107,7 +107,7 @@ class NewsRenderer {
     const button = document.createElement('button');
     button.classList.add('news-accordion-btn');
     button.innerHTML = `
-      Показать все (${totalNews - defaultVisible})
+      Показать ещё (${totalNews - defaultVisible})
       <svg class="accordion-icon" viewBox="0 0 24 24">
         <path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z"/>
       </svg>
@@ -164,7 +164,7 @@ class NewsRenderer {
         });
         
         button.innerHTML = `
-          Показать все (${totalNews - defaultVisible})
+          Показать ещё (${totalNews - defaultVisible})
           <svg class="accordion-icon" viewBox="0 0 24 24">
             <path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z"/>
           </svg>
