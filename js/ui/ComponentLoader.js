3f4b7f79c24a67a586518a3c3771743ea62335ab
neutrall85 <canni85@mail.ru>
2026-04-15 13:25:54 +0400

    modified:   js/ui/ComponentLoader.js

--- js/ui/ComponentLoader.js
+++ js/ui/ComponentLoader.js
@@ -264,7 +264,7 @@ const ComponentLoader = {
       <svg viewBox="0 0 24 24"><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/></svg>
     </button>
     <div class="modal-header">
-      <h2 class="modal-title" id="vacanciesModalTitle">Оставить заявку</h2>
+      <h2 class="modal-title" id="vacanciesModalTitle">Отклик на вакансию</h2>
       <p class="modal-subtitle">Заполните форму ниже, и мы рассмотрим вашу кандидатуру</p>
     </div>
     <div class="modal-body">
